08c4f38eec369f75560bb5192570a1beed1a46fe
lengbingkeji <1517699467@qq.com>
2024-01-08 14:45:34 +0800

    新增 “lbkj-item-service” 微服务

---
+++ lbkj-item-service/src/main/java/com/lengbingkeji/cloud/Main.java
@@ -0,0 +1,16 @@
+package com.lengbingkeji.cloud;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.lengbingkeji.cloud
+ * Description:
+ *
+ * @Author lengbingkeji
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
